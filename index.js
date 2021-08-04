c7c0a002b1c08346f44eb9d7dc4196c4eb6dc38f
huutrong1101 <75382199+huutrong1101@users.noreply.github.com>
2021-08-04 21:06:00 +0700

    upload

---
+++ index.js
@@ -0,0 +1,38 @@
+let now=new Date();
+let kn= new Date("2016-2-29");
+let ngay=Math.ceil((now-kn)/(60*60*24*1000));
+console.log(ngay);
+document.querySelector(".times").innerHTML=ngay +" days";
+
+
+let menu = document.querySelector("#menu-bar");
+let navbar = document.querySelector(".nav");
+window.onscroll=()=>{
+    menu.classList.remove("fa-times");
+    navbar.classList.remove("active");
+}
+menu.addEventListener("click",()=> {
+    menu.classList.toggle("fa-times");
+    navbar.classList.toggle("active");
+})
+
+
+var swiper = new Swiper(".memoriesSwiper", {
+    spaceBetween:20,
+    loop: true,
+    autoplay:{
+        delay:2500,
+        disableOnInteraction: false,
+    },
+    breakpoints:{
+        640:{
+            slidesPerView:1,
+        },
+        768:{
+            slidesPerView:2,
+        },
+        1024:{
+            slidesPerView:3,
+        }
+    }
+});
