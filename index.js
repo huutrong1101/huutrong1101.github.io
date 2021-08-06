9a2de138bd48ac8812997e6fd398de55589440a1
huutrong1101 <75382199+huutrong1101@users.noreply.github.com>
2021-08-06 17:03:54 +0700

    upload

--- index.js
+++ index.js
@@ -10,6 +10,16 @@ window.onscroll=()=>{
     menu.classList.remove("fa-times");
     navbar.classList.remove("active");
 }
+function loader(){
+    document.querySelector('.loader-container').classList.add('fade-out');
+}
+function fadeOut(){
+    setInterval(loader,3000);
+}
+window.onload = fadeOut();
+
+
+
 menu.addEventListener("click",()=> {
     menu.classList.toggle("fa-times");
     navbar.classList.toggle("active");
